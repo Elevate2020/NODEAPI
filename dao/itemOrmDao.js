2b3c99a77ad1d04b28446b2f44c3f99d33043069
swarajbiswal <swaraj.biswal@target.com>
2020-05-13 23:43:05 +0530

    ORM DAO

---
+++ dao/itemOrmDao.js
@@ -0,0 +1,30 @@
+const { Item } = require('./sequelize')
+
+/** Get all Items from DB */
+function getAllItems(response) {
+    console.log( 'Inside ORM DB access...');
+    Item.findAll().then(items => response.json(items))
+}
+
+/** Get Item based in Item id */
+function getItemsById(itemId, response) {
+    console.log( 'Inside ORM DB access...   '+itemId);
+    if(itemId){
+        Item.findAll({ where: { id: itemId } }).then(item => response.json(item));
+    }else{
+        Item.findAll().then(items => response.json(items))
+    }
+}
+
+/** Add Item to database */
+function addItem(jsonRequest, response) {
+    console.log('Add Item Dao'+jsonRequest)
+    Item.create(jsonRequest)
+        .then(item => response.json(item))
+}
+
+module.exports = {
+    getAllItems,
+    addItem,
+    getItemsById
+  }
